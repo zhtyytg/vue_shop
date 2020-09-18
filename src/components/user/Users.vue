188da5e8c7b3b010721cb1569f26d1ed063728eb
zhtyytg <1143939519@qq.com>
2020-09-18 10:15:55 +0800

    完成 添加用户对话框

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -112,15 +112,37 @@
     </el-card>
 
     <!-- 添加用户对话框 -->
-    <el-dialog title="提示" :visible.sync="addDialogVisible" width="50%">
+    <el-dialog
+      title="提示"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed"
+    >
       <!-- 内容主体区 -->
-      <span>这是一段信息</span>
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
       <!-- 底部区域 -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false"
-          >确 定</el-button
-        >
+        <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -129,6 +151,25 @@
 <script>
 export default {
   data() {
+    var checkEmail = (rule, value, cb) => {
+      // 验证邮箱的正则表达式
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        // 合法邮箱
+        return cb()
+      }
+      cb(new Error('请输入合法的邮箱'))
+    }
+    var checkMobile = (rule, value, cb) => {
+      // 验证手机号的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+
+      if (regMobile.test(value)) {
+        // 合法手机号
+        return cb()
+      }
+      cb(new Error('请输入合法的手机号'))
+    }
     return {
       // 获取用户列表的参数对象
       queryInfo: {
@@ -141,7 +182,47 @@ export default {
       userList: [],
       total: 0,
       // 控制添加用户对话框的显示与隐藏
-      addDialogVisible: false
+      addDialogVisible: false,
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 添加表单的验证规则对象
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', triger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '用户名长度在3~10个字符之间',
+            triger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', triger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: '密码长度在6~15个字符之间',
+            triger: 'blur'
+          }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', triger: 'blur' },
+          { validator: checkEmail, message: '请输入合法邮箱', triger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号码', triger: 'blur' },
+          {
+            validator: checkMobile,
+            message: '请输入合法手机号',
+            triger: 'blur'
+          }
+        ]
+      }
     }
   },
   created() {
@@ -157,7 +238,7 @@ export default {
         return this.$message.error('获取用户列表失败')
       this.userList = res.data.users
       this.total = res.data.total
-      console.log(res)
+      // console.log(res)
     },
     // 监听 pagesize 改变的事件
     handleSizeChange(newSize) {
@@ -182,6 +263,26 @@ export default {
         return this.$message.error('更新用户状态失败')
       }
       this.$message.success('更新用户状态成功')
+    },
+    // 监听添加用户对话框的关闭事件
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮，添加新用户
+    addUser() {
+      this.$refs.addFormRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return
+        // 发起添加用户的请求
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) {
+          this.$message.error('添加用户失败')
+        }
+        this.$message.success('添加用户成功')
+        this.addDialogVisible = false
+        // 重新获取用户列表
+        this.getUserList()
+      })
     }
   }
 }
